d5dd7c1a57f9a1c1592faf97f6b72399d7fa2d4b
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-04-01 09:31:03 +0600

    source conntected with secret pass

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSOWRD}@cluster0.tdy11iu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+          const collection = client.db("test").collection("devices");
+          // perform actions on the collection object
+          console.log('database connected');
+          client.close();
+});
+
+
+
+app.get('/', (req, res) => {
           res.send('Genius server is runnig')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
           console.log('server is running!');
 })
